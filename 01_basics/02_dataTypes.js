7cdbe64da2ce49d1215e8924b78e8674305feda5
Muhammad Usman <168564659+sheikh-usman-38@users.noreply.github.com>
2025-07-17 08:38:27 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all Js code as newer version
+
+//alert(3+3) // we are using node.js, not browser So its not run Here.
+console.log(
+    3+
+    3
+) //its bad practice | Code Readability issue.
+//So writing code like this 
+console.log("usman");
+
+let name="usman"; //String
+let age=18; //Number
+let isLoggedIn = false; //Boolean
+
+//Primitive DataType
+1. //number =>2 to power 53
+2. //bigint used for large value
+3. //string =>""
+4. //boolean => true/false
+5. //null => standalone value   /* typeOf NULL is Object */
+6. //undefined =>
+7. //symbol => unique
+
+//Object
+console.log(typeof name);//String
+console.log(typeof null);//Object (this is a known JavaScript quirk)
+console.log(typeof age);   // "number"
+console.log(typeof isLoggedIn); // "boolean"
+
+
