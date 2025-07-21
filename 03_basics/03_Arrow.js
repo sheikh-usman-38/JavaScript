e1c1ee7f2bd75379d57d6e8c24f3b8f1c24bec4b
Muhammad Usman <168564659+sheikh-usman-38@users.noreply.github.com>
2025-07-21 13:37:20 +0000

    ArrowFunction

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -10,11 +10,58 @@ const user={
 
     }
 }
-user.welcomeMsg();
+//user.welcomeMsg();
 user.userName ="Hussan";
-user.welcomeMsg(); //Hussan, welcome to our website
+//user.welcomeMsg(); //Hussan, welcome to our website
 
 
 // Because Here is not context of any object
-console.log(this); //{}
+//console.log(this); //{}
 // In Browser have global object is window 
+
+
+// this keyword is not work in function
+// function chai(){
+//     let username= "Dawood";
+//     console.log(this.username); // output is undefined
+//     //So this keyword not access not current context value of function
+// }
+// chai();
+
+// Another way to check  store function in variable
+
+const chai = function (){
+    let username= "Dawood";
+    console.log(this.username); // output is undefined
+    //So this keyword not access not current context value of function
+}
+chai(); // Again output is undefined
+
+//********************Arrow funtion (ES6) ***********/
+//Arrow function is a short way to write a function in ES6 (ES2015)
+
+const  tea =()=>{
+    let username= "Dawood";
+    console.log(this.username); 
+}
+tea(); //undefined
+
+// const addVal =(num1 ,num2)=>{
+//   return num1+num2
+// }
+//console.log(addVal(5,6));   //    11
+
+
+//   Implicit Return
+// When a function returns a value automatically without using the return keyword,
+//  it’s called implicit return.
+
+//const addVal =(num1 ,num2)=> num1+num2
+
+//also write this for this way
+//const addVal =(num1 ,num2)=> (num1+num2)
+
+//Return  Object  in arrow function so write code like this 
+const addVal =(num1 ,num2)=> ({name:"usman"})
+
+console.log(addVal(5,6));  { name: 'usman' };
