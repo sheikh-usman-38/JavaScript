2041b7702276aec69a136335c0765246d79f822f
Muhammad Usman <168564659+sheikh-usman-38@users.noreply.github.com>
2025-07-21 06:46:02 +0000

    Function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -41,3 +41,58 @@ function loginUserMessage(username="Dawood"){
 }
 //console.log(loginUserMessage("Usman"));//  Usman your are loggedIn
 console.log(loginUserMessage());//  undefined your are loggedIn
+
+
+
+//***************************Rest Operator******************************/
+//The rest operator in JavaScript (...) is used to collect multiple elements into a single array.
+
+// Difference between Rest and Spread Operator
+/*Feature	                  Rest Operator	                 Spread Operator
+Purpose	                Collects values into an array	    Spreads values out from an array/object
+Used In           	Function parameters, destructuring	     Function calls, array/object copying
+Syntax	                   function sum(...nums)	           console.log(...arr)
+Direction	          Collects multiple values → into one	     Spreads one value → into multiple
+*/
+
+
+//Rest operator right Like This (...num1) Function parameters
+
+function calculateCartPrice(...num1){
+  return num1;
+}
+//console.log(calculateCartPrice(1,4,5,8)); // [ 1, 4, 5, 8 ]
+
+// So here is parameter val1 ,val2 is not print other value value make array and print
+function calculateCartPrice(val1,val2,...num1){
+  return num1;
+}
+//console.log(calculateCartPrice(100,400,800,500)); //[ 800, 500 ]
+
+
+//Access object in function
+
+// //let user={
+//     name:"usman",
+//     price:"50k",
+// }
+function addUser(anyUser){
+    console.log(`username is ${anyUser.name} and price is ${anyUser.price}`);
+}
+//WE pass argument as name of object 
+//addUser(user);  //username is usman and price is 50k
+
+//all pass object as a argument
+addUser({
+    name:"Ahmad",
+    price:"30k"
+}) //username is Ahmad and price is 30k
+
+////Access array in function
+const myArr =[100,500,300];
+function arrayVal(getVal){
+    return getVal; //Also get individual value ==> getVal[1]
+}
+console.log(arrayVal(myArr)); //    [ 100, 500, 300 ]
+////Also pass array as a argument
+console.log(arrayVal([700,400,200])); //[ 700, 400, 200 ]
