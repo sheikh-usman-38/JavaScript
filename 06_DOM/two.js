75ccd619d97366cc61bec2bb00fe8415b83e7f8b
Muhammad Usman <168564659+sheikh-usman-38@users.noreply.github.com>
2025-07-25 13:40:13 +0000

    Dom

---
+++ 06_DOM/two.js
@@ -0,0 +1,19 @@
+/*
+document.getElementById("title");   // access id 
+document.getElementById('title').getAttribute('class'); //'heading'
+document.getElementById('title').setAttribute('class',"text heading");   // change class name 
+document.getElementById('title').className    //heading  <-> className print
+document.getElementById('title').id // title   <->  id Name print
+document.getElementsByClassName(" heading")  
+
+title.textContent   //also access hidden text like any element who is hidden in html file mean display none
+ output:  'DOM Learning on Chai or Code \n    test writing'
+
+title.innerText   // only access of that element who show in display|Only returns visible (displayed)text on the page.
+output: 'DOM Learning on Chai or Code'
+
+title.innerHTML //This returns the HTML content (including tags) inside the element with id="title".
+output :  'DOM Learning on Chai or Code <span style="display: none;">\n  Scrite text</span>'
+
+
+*/
